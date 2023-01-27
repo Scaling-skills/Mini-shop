9414332c4a81ce09893a03866cfda4831735706f
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-01-27 19:58:51 +0100

    -

--- MyApp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,7 +16,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
