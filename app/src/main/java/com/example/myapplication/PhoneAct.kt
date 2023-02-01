7eae077c9cefc495e2180cdebce58bb805acd65d
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-01 17:44:08 +0100

    Update PhoneAct.kt

--- app/src/main/java/com/example/myapplication/PhoneAct.kt
+++ app/src/main/java/com/example/myapplication/PhoneAct.kt
@@ -5,14 +5,104 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSecondBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 
 class PhoneAct : AppCompatActivity() {
+
+    private lateinit var sendOTPBn : Button
+    private lateinit var phoneNumberET : EditText
+    private lateinit var auth : FirebaseAuth
+    private lateinit var number : String
+
+
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.phone_act)
+
+        init()
+        sendOTPBn.setOnClickListener {
+            number = phoneNumberET.text.trim().toString()
+            if (number.isNotEmpty()){
+                if (number.length == 9){
+                    number = "+48$number"
+
+                    val options = PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber(number)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                        .build()
+                    PhoneAuthProvider.verifyPhoneNumber(options)
+
+                }else{
+                    Toast.makeText(this , "incorrect number" , Toast.LENGTH_SHORT).show()
+                }
+
+            }else{
+                Toast.makeText(this , "Enter number" , Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+    }
+
+    private fun init(){
+        sendOTPBn = findViewById(R.id.sendOTPBtn)
+        phoneNumberET = findViewById(R.id.phoneEditTextNumber)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+            Log.d(TAG, "onVerificationCompleted:$credential")
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            Log.w(TAG, "onVerificationFailed", e)
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            Log.d(TAG, "onCodeSent:$verificationId")
+
+            // Save verification ID and resending token so we can use them later
+            storedVerificationId = verificationId
+            resendToken = token
+        }
     }
 }
