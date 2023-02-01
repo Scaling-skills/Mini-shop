43c8c30bd1c0375a68615f2e7eab6aa583e2e867
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-01 10:35:39 +0100

    powershell path

---
+++ app/src/main/java/com/example/myapplication/PhoneAct.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentSecondBinding
+
+
+class PhoneAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.PhoneAct)
+    }
+}
