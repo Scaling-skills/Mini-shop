1ae39531ffede2c170124cf24dabafcf82ff6968
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-01 18:19:18 +0100

    Update PhoneAct.kt

--- app/src/main/java/com/example/myapplication/PhoneAct.kt
+++ app/src/main/java/com/example/myapplication/PhoneAct.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSecondBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.BrokenBarrierException
 import java.util.concurrent.TimeUnit
 
 
@@ -24,6 +29,7 @@ class PhoneAct : AppCompatActivity() {
     private lateinit var phoneNumberET : EditText
     private lateinit var auth : FirebaseAuth
     private lateinit var number : String
+    private lateinit var mProgressBar : ProgressBar
 
 
 
@@ -39,10 +45,10 @@ class PhoneAct : AppCompatActivity() {
                     number = "+48$number"
 
                     val options = PhoneAuthOptions.newBuilder(auth)
-                        .setPhoneNumber(number)       // Phone number to verify
-                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-                        .setActivity(this)                 // Activity (for callback binding)
-                        .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                        .setPhoneNumber(number)
+                        .setTimeout(60L, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(callbacks)
                         .build()
                     PhoneAuthProvider.verifyPhoneNumber(options)
 
@@ -59,50 +65,57 @@ class PhoneAct : AppCompatActivity() {
     }
 
     private fun init(){
+        mProgressBar = findViewById(R.id.phoneProgressBar)
+        mProgressBar.visibility = View.INVISIBLE
         sendOTPBn = findViewById(R.id.sendOTPBtn)
         phoneNumberET = findViewById(R.id.phoneEditTextNumber)
         auth = FirebaseAuth.getInstance()
     }
 
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+
+                } else {
+
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+
+                    }
+
+                }
+            }
+    }
+
+
     val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
         override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-            // This callback will be invoked in two situations:
-            // 1 - Instant verification. In some cases the phone number can be instantly
-            //     verified without needing to send or enter a verification code.
-            // 2 - Auto-retrieval. On some devices Google Play services can automatically
-            //     detect the incoming verification SMS and perform verification without
-            //     user action.
-            Log.d(TAG, "onVerificationCompleted:$credential")
+
+
             signInWithPhoneAuthCredential(credential)
         }
 
         override fun onVerificationFailed(e: FirebaseException) {
-            // This callback is invoked in an invalid request for verification is made,
-            // for instance if the the phone number format is not valid.
-            Log.w(TAG, "onVerificationFailed", e)
+
+
 
             if (e is FirebaseAuthInvalidCredentialsException) {
                 // Invalid request
             } else if (e is FirebaseTooManyRequestsException) {
-                // The SMS quota for the project has been exceeded
+
             }
 
-            // Show a message and update the UI
+
         }
 
         override fun onCodeSent(
             verificationId: String,
             token: PhoneAuthProvider.ForceResendingToken
         ) {
-            // The SMS verification code has been sent to the provided phone number, we
-            // now need to ask the user to enter the code and then construct a credential
-            // by combining the code with a verification ID.
-            Log.d(TAG, "onCodeSent:$verificationId")
 
-            // Save verification ID and resending token so we can use them later
-            storedVerificationId = verificationId
-            resendToken = token
+
         }
     }
 }
