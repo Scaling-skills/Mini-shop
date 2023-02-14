38424320bcd3900ea62c24f1a46daef71e8140ea
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-14 10:46:51 +0100

    Update ViewModel.kt

--- app/src/main/java/com/example/myapplication/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/ViewModel.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.myapplication.R
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.model.DataModel
+
 
 class ViewModel {
 }
