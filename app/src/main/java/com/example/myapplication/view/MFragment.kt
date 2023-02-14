2786602f955104b17dee7c9acddbc7665438a5a8
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-14 14:24:50 +0100

    Update MFragment.kt

--- app/src/main/java/com/example/myapplication/view/MFragment.kt
+++ app/src/main/java/com/example/myapplication/view/MFragment.kt
@@ -1,4 +1,18 @@
 package com.example.myapplication.view
 
-class MFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentBinding
+import com.example.myapplication.viewmodel.ViewModel
+
+class MFragment: Fragment() {
+
+    private var _binding: FragmentBinding? = null
+    private val binding get() = _binding!!
 }
