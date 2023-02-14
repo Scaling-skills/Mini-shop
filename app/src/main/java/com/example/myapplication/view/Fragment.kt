179582c6d44f5a685b9565eecd85f2db4ee92df9
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-14 08:29:34 +0100

    -

--- app/src/main/java/com/example/myapplication/view/Fragment.kt
+++ app/src/main/java/com/example/myapplication/view/Fragment.kt
@@ -1,9 +1,14 @@
 package com.example.myapplication.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.example.myapplication.databinding.FragmentBinding
+import com.example.myapplication.viewmodel.ViewModel
 import com.example.myapplication.R
 
 class Fragment {
