f623c3dea89d16a96e1fbbc0b9ba563dfa89b932
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-14 07:47:30 +0100

    -

--- app/src/main/java/com/example/myapplication/model/DataModel.kt
+++ app/src/main/java/com/example/myapplication/model/DataModel.kt
@@ -1,4 +1,10 @@
 package com.example.myapplication.model
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.myapplication.R
+
 class DataModel {
 }
--- app/src/main/java/com/example/myapplication/view/Fragment.kt
+++ app/src/main/java/com/example/myapplication/view/Fragment.kt
@@ -1,4 +1,10 @@
 package com.example.myapplication.view
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.myapplication.R
+
 class Fragment {
 }
