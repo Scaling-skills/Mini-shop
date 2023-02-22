b48441eb342fa3173421ce4dfd0efdd0c3d65acd
Scaling-skills <a.adam.wodzinski@gmail.com>
2023-02-22 12:46:32 +0100

    Update MFragment.kt

--- app/src/main/java/com/example/myapplication/view/MFragment.kt
+++ app/src/main/java/com/example/myapplication/view/MFragment.kt
@@ -9,10 +9,29 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentBinding
+import com.example.myapplication.model.DataModel
 import com.example.myapplication.viewmodel.ViewModel
 
 class MFragment: Fragment() {
 
     private var _binding: FragmentBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ViewModel by activityViewModels()
+
+    override fun OnCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+
+
+
+
+
 }
